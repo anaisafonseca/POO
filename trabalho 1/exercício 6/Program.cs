e32d03791f7986d8ac4e89bdba2666bfc3bb3a90
Anaísa Fonseca <39317768+anaisafonseca@users.noreply.github.com>
2020-11-29 15:31:25 -0300

    Add files via upload

---
+++ trabalho 1/exercício 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caVetor3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor3D a = new Vetor3D();
+            Vetor3D b = new Vetor3D();
+            Vetor3D c = new Vetor3D();
+
+            Console.WriteLine("Digite a componente x do vetor B:");
+            b.setX(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite a componente y do vetor B:");
+            b.setY(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite a componente z do vetor B:");
+            b.setZ(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Modulo do vetor a: " + a.Modulo3D());
+            Console.WriteLine("Modulo do vetor b: " + b.Modulo3D());
+            c = a.Vetorial(b);
+            Console.WriteLine("O produto vetorial de a e b é: (" + c.getX() + ", " + c.getY() + ", " + c.getZ() + ")");
+            Console.ReadLine();
+
+        }
+    }
+}
