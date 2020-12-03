cbd9aa1faedded60831af70bde024d5d5678849b
Anaísa Fonseca <39317768+anaisafonseca@users.noreply.github.com>
2020-12-03 19:11:44 -0300

    Add files via upload

---
+++ caRetangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace caRETANGULO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how i love you, i can´t love myself");
+
+            Retangulo r1 = new Retangulo();  //criei um objeto da classe retangulo
+            Retangulo r2 = new Retangulo();  //criei outro objeto da classe retangulo
+            Retangulo r3 = new Retangulo(5.0, 8.0);
+            Retangulo r4 = new Retangulo(10, 5);
+
+            Console.WriteLine("Area do retangulo 1: " + r1.Area());  //1
+            Console.WriteLine("Base do retangulo 1: " + r1.getBasis());  //1
+            Console.ReadLine();
+
+            r1.setBasis(4.0);
+            r1.setHeight(5.0);
+            r2.setBasis(5.0);
+            r2.setHeight(6.0);
+
+            Console.WriteLine("Area do retangulo 1: " + r1.Area());  //20
+            Console.WriteLine("Base do retangulo 1: " + r1.getBasis());  //4
+            Console.ReadLine();
+
+            Console.WriteLine("Area do retangulo 2: " + r2.Area());  //30
+            Console.WriteLine("Area do retangulo 3: " + r3.Area());  //40
+            Console.WriteLine("Area do retangulo 4: " + r4.Area());  //50
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ caRetangulo/Retangulo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caRETANGULO
+{
+    class Retangulo
+    {
+        //atributos
+        private double basis;
+        private double height;
+
+        //métodos
+        public Retangulo()
+        {
+            basis = 1.0;
+            height = 1.0;
+        }
+
+        public Retangulo(double b, double h)
+        {
+            basis = b;
+            height = h;
+        }
+
+        public double Area()
+        {
+            double area;
+            area = basis * height;
+            return area;
+        }
+
+        public void setBasis(double new_basis)  //altera o valor da base
+        {
+            basis = new_basis;
+        }
+        public void setHeight(double new_height)  //altera o valor da altura
+        {
+            height = new_height;
+        }
+
+        public double getBasis()  //retorna o valor da base
+        {
+            return basis;
+        }
+        public double getHeight()  //retorna o valor da altura
+        {
+            return height;
+        }
+    }
+}
