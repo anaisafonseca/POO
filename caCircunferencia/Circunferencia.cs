bead5412efa1bfe5389bc49bf8ad093fc0394ea4
Anaísa Fonseca <39317768+anaisafonseca@users.noreply.github.com>
2020-12-03 19:13:05 -0300

    Add files via upload

---
+++ caCircunferencia/Circunferencia.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caCIRCUNFERENCIA
+{
+    class Circunferencia
+    {
+        //atributo
+        private double radius;
+
+        //métodos
+        public Circunferencia()
+        {
+            radius = 1.0;
+        }
+
+        public Circunferencia(double r)
+        {
+            radius = r;
+        }
+
+        double pi = Math.PI;
+
+        public double Area()
+        {
+            double area;
+            area = pi * radius * radius;
+            return area;
+        }
+
+        public double Perimetro()
+        {
+            double perimetro;
+            perimetro = 2 * pi * radius;
+            return perimetro;
+        }
+
+        public void setRadius(double new_radius)
+        {
+            radius = new_radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+    }
+}
