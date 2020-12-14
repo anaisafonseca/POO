f93fb0aa92046f1436260900fa35852bc664eefd
Anaísa Fonseca <39317768+anaisafonseca@users.noreply.github.com>
2020-12-14 20:33:42 -0300

    Update Program.cs

--- caPolimorfismo/Program.cs
+++ caPolimorfismo/Program.cs
@@ -11,14 +11,10 @@ namespace caPolimorfismo
         static void Main(string[] args)
         {
 
-            Veiculo[] array_veiculos = new Veiculo[2]; // criando um array de 2 Veiculos
-
+            Veiculo[] array_veiculos = new Veiculo[2];
             Automovel v1 = new Automovel("SPIN", 4);
-
             array_veiculos[0] = v1;
-
             Aeronave v2 = new Aeronave("AIRBUS", 2);
-
             array_veiculos[1] = v2;
 
             for(int i = 0; i < array_veiculos.Length; i++)
@@ -26,11 +22,8 @@ namespace caPolimorfismo
                 array_veiculos[i].mover();
                 array_veiculos[i].parar();
             }
-
+            
             Console.ReadLine();
-
-
-
         }
     }
 }
